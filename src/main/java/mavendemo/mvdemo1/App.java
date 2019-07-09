54a8912ea1f715adf7b9b556b48ebafd6c1a8278
jkchen-hub <1054109772@qq.com>
2019-07-09 14:10:34 +0800

    demo2

---
+++ src/main/java/mavendemo/mvdemo1/App.java
@@ -0,0 +1,15 @@
+package mavendemo.mvdemo1;
+import org.apache.commons.math3.util.ArithmeticUtils;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	int a = ArithmeticUtils.gcd(456,28);
+    	System.out.println(a);
+        System.out.println( "Hello World!" );
+    }
+}
