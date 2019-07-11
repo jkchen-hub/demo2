bba496aeabc1a50db2a76bd57167784655d07dfe
jkchen-hub <1054109772@qq.com>
2019-07-11 09:22:06 +0800

    changed

--- src/main/java/mavendemo/mvdemo1/App.java
+++ src/main/java/mavendemo/mvdemo1/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-    	int a = ArithmeticUtils.gcd(456,28);
+    	int a = ArithmeticUtils.gcd(456,64);  //计算两个数的最大公约数
     	System.out.println(a);
         System.out.println( "Hello World!" );
         
